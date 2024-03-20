dd658f1653cab5ca34f4cc544117dde8ca3630a1
DarknessReap <wto9@myseneca.ca>
2024-03-19 20:09:43 -0400

    Overview finished
    I finished overview page and as for ticket page, I only did the sidebar navigation.

---
+++ Public/main.js
@@ -0,0 +1,5 @@
+// JavaScript to add 'move-right' class to the content column
+document.addEventListener("DOMContentLoaded", function() {
+    var contentColumn = document.querySelector('.col-md-9');
+    contentColumn.classList.add('move-right');
+});
